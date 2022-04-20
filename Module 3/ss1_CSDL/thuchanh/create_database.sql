09b5113b5ec6c935db928749873a810eaca8f5b3
nguyenvansy1 <ashewarakill2@gmail.com>
2022-04-20 20:53:12 +0700

    .

---
+++ Module 3/ss1_CSDL/thuchanh/create_database.sql
@@ -0,0 +1,7 @@
+create database module3ss1;
+studentstudentidstudentuse module3ss1;
+create table student(
+ id int,
+`name` nvarchar(20),
+ age int
+ );
---
+++ README.md
@@ -0,0 +1,4 @@
+# nguyenvansy-A1121I1
+# A1121I1-Nguyen_Van_Sy
+# A1121I1-Nguyen_Van_Sy
+# A1121I1-Nguyen_Van_Sy
