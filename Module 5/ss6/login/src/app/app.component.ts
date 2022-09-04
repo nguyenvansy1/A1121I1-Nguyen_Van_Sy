654c54d256d030b74459545231de4461569af4d3
nguyenvansy1 <ashewarakill2@gmail.com>
2022-09-04 13:04:12 +0700

    .

---
+++ Module 5/ss6/login/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'login';
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
